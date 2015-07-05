40b7ceea5536def0f9d403242ed2bd2ffec2190c
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-07-05 10:24:09 +0200

    Use newest framework and newest sbutil

--- src/test/java/dk/statsbiblioteket/medieplatform/autonomous/newspaper/RestartWorkflowTest.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/autonomous/newspaper/RestartWorkflowTest.java
@@ -2,7 +2,7 @@ package dk.statsbiblioteket.medieplatform.autonomous.newspaper;
 
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedora;
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
-import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
+import dk.statsbiblioteket.sbutil.webservices.authentication.Credentials;
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.ConfigConstants;
 import dk.statsbiblioteket.medieplatform.autonomous.Event;
